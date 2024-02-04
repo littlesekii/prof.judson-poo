a874214910335cd4eba395c1d6c81f1967dad087
littlesekii <littlesekii@gmail.com>
2024-02-04 12:52:47 -0300

    Iniciando o Lab 06

---
+++ Labs/Lab06/ExFix02/main.cpp
@@ -0,0 +1,10 @@
+#include "Jogo.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	Jogo sackboy{ "Sackboy", 150.0f };
+	Jogo spiderman{ "Spiderman", 200.0f };
+	Jogo horizon{ "Horizon", 199.0f };
+}
---
+++ Labs/Lab06/ExFix03/main.cpp
@@ -0,0 +1,9 @@
+#include "Jogo.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const Jogo ratchet{ "Ratchet & Clank", 150.0f };
+	ratchet.exibir(); // não modifica 
+}
